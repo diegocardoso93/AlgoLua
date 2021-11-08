1d666926f63c98903833bdf6fdd380dec5af3ec9
diegocardoso93 <dieg0card0s0@hotmail.com>
2021-11-07 22:00:14 -0300

    indexer, kmd

--- Indexer.lua
+++ Indexer.lua
@@ -11,181 +11,288 @@ local Indexer = {
 	token = nil
 }
 
-function headers()
+local function headers()
 	return { ["x-api-key"] = Indexer.token }
 end
 
+-- @Method:
+--  Indexer.health
+--  Returns 200 if healthy.
+-- @Input:
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.health(on_success, on_error)
 	http_client.get(Indexer.address .. "/health", headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.accounts
+--  Search for accounts.
+-- @Input:
+--  - address: string
+--  - params: table {
+--    application-id: integer,
+--    asset-id: integer,
+--    auth-addr: string,
+--    currency-greater-than: integer,
+--    currency-less-than: integer,
+--    include-all: boolean,
+--    limit: integer,
+--    next: string,
+--    round: integer
+--  } | nil
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.accounts(on_success, on_error)
-	--[[
-	@Optional parameters:
-	application-id: integer,
-	asset-id: integer,
-	auth-addr: string
-	currency-greater-than: integer
-	currency-less-than: integer
-	include-all: boolean
-	limit: integer
-	next: string
-	round: integer
-	]]
 	http_client.get(Indexer.address .. "/v2/accounts", headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.accounts
+--  Lookup account information.
+-- @Input:
+--  - address: string
+--  - params: table {
+--    include-all: boolean,
+--    round: integer
+--  } | nil
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.account(account_id, on_success, on_error)
-	--[[
-	@Optional parameters:
-	include-all: boolean,
-	round: integer
-	]]
 	http_client.get(Indexer.address .. "/v2/accounts/" .. account_id, headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.account_transactions
+--  Lookup account transactions.
+-- @Input:
+--  - account_id: string
+--  - params: table {
+--    after-time: string (date-time),
+--    asset-id: boolean,
+--    before-time: string (date-time),
+--    currency-greater-than: integer,
+--    currency-less-than: integer,
+--    limit: integer,
+--    max-round: integer,
+--    min-round: integer,
+--    next: string,
+--    note-prefix: string,
+--    rekey-to: boolean,
+--    round: integer,
+--    sig-type: enum (sig, msig, lsig),
+--    tx-type: enum (pay, keyreg, acfg, axfer, afrz, appl),
+--    txid: string
+--  } | nil
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.account_transactions(account_id, on_success, on_error)
-	--[[
-	@Optional parameters:
-	after-time: string(date-time),
-	asset-id: integer,
-	before-time: string(date-time),
-	currency-greater-than: integer,
-	currency-less-than: integer,
-	limit: integer,
-	max-round: integer,
-	min-round: integer,
-	next: string,
-	note-prefix: string,
-	rekey-to: boolean,
-	round: integer,
-	sig-type: enum (sig, msig, lsig),
-	tx-type: enum (pay, keyreg, acfg, axfer, afrz, appl),
-	txid: string,
-	]]
 	http_client.get(Indexer.address .. "/v2/accounts/" .. account_id .. "/transactions", headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.applications
+--  Search for applications.
+-- @Input:
+--  - account_id: string
+--  - params: table {
+--    include-all: boolean,
+--    limit: integer,
+--    next: string,
+--  } | nil
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.applications(on_success, on_error)
-	--[[
-	@Optional parameters:
-	include-all: boolean,
-	limit: integer,
-	next: string,
-	]]
 	http_client.get(Indexer.address .. "/v2/applications", headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.application
+--  Lookup application.
+-- @Input:
+--  - application_id: integer
+--  - params: table {
+--    include-all: boolean
+--  } | nil
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.application(application_id, on_success, on_error)
-	--[[
-	@Optional parameters:
-	include-all: boolean,
-	]]
 	http_client.get(Indexer.address .. "/v2/applications/" .. application_id, headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.application
+--  Lookup application logs.
+-- @Input:
+--  - application_id: integer
+--  - params: table {
+--    limit: integer,
+--    max-round: integer,
+--    min-round: integer,
+--    next: string,
+--    sender-address: string,
+--    txid: string
+--  } | nil
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.application_logs(application_id, on_success, on_error)
-	--[[
-	@Optional parameters:
-	limit: integer,
-	max-round: integer,
-	min-round: integer,
-	next: string,
-	sender-address: string,
-	txid: string,
-	]]
 	http_client.get(Indexer.address .. "/v2/applications/" .. application_id .. "/logs", headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.assets
+--  Search for assets.
+-- @Input:
+--  - application_id: integer
+--  - params: table {
+--    asset-id: integer,
+--    creator: string,
+--    include-all: boolean,
+--    limit: integer,
+--    next: string,
+--    unit: string,
+--  } | nil
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.assets(on_success, on_error)
-	--[[
-	@Optional parameters:
-	asset-id: integer,
-	creator: string,
-	include-all: boolean,
-	limit: integer,
-	name: string,
-	next: string,
-	unit: string,
-	]]
 	http_client.get(Indexer.address .. "/v2/assets", headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.asset
+--  Lookup asset information.
+-- @Input:
+--  - application_id: integer
+--  - params: table {
+--    include-all: boolean,
+--  } | nil
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.asset(asset_id, on_success, on_error)
-	--[[
-	@Optional parameters:
-	include-all: string,
-	]]
 	http_client.get(Indexer.address .. "/v2/assets/" .. asset_id, headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.asset_balances
+--  Lookup the list of accounts who hold this asset.
+-- @Input:
+--  - application_id: integer
+--  - params: table {
+--    currency-greater-than: integer,
+--    currency-less-than: integer,
+--    include-all: boolean,
+--    limit: integer,
+--    next: string,
+--    round: integer,
+--  } | nil
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.asset_balances(asset_id, on_success, on_error)
-	--[[
-	@Optional parameters:
-	currency-greater-than: integer,
-	currency-less-than: integer,
-	include-all: boolean,
-	limit: integer,
-	next: string,
-	round: integer,
-	]]
 	http_client.get(Indexer.address .. "/v2/assets/" .. asset_id .. "/balances", headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.asset_transactions
+--  Lookup transactions for an asset.
+-- @Input:
+--  - asset_id: integer
+--  - params: table {
+--    address: string,
+--    aaddress-role: enum (sender, receiver, freeze-target),
+--    after-time: string (date-time),
+--    before-time: string (date-time),
+--    currency-greater-than: integer,
+--    currency-less-than: integer,
+--    exclude-close-to: boolean,
+--    limit: integer,
+--    max-round: integer,
+--    min-round: integer,
+--    next: string,
+--    note-prefix: string,
+--    rekey-to: boolean,
+--    round: integer,
+--    rekey-to: boolean,
+--    sig-type: enum (sig, msig, lsig),
+--    tx-type: enum (pay, keyreg, acfg, axfer, afrz, appl),
+--    txid: string,
+--  } | nil
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.asset_transactions(asset_id, on_success, on_error)
-	--[[
-	@Optional parameters:
-	address: integer,
-	address-role: enum (sender, receiver, freeze-target),
-	after-time: string (date-time),
-	before-time: string (date-time),
-	currency-greater-than: integer,
-	currency-less-than: integer,
-	exclude-close-to: boolean,
-	limit: integer,
-	max-round: integer,
-	min-round: integer,
-	next: string,
-	note-prefix: string,
-	rekey-to: boolean,
-	round: integer,
-	sig-type: enum (sig, msig, lsig),
-	tx-type: enum (pay, keyreg, acfg, axfer, afrz, appl),
-	txid: string,
-	]]
 	http_client.get(Indexer.address .. "/v2/assets/" .. asset_id .. "/transactions", headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.block
+--  Lookup block.
+-- @Input:
+--  - round_number: integer
+--  - params: table {
+--    address: integer,
+--    address-role: enum (sender, receiver, freeze-target),
+--    after-time: string (date-time),
+--    application-id: integer,
+--    asset-id: integer,
+--    before-time: string (date-time),
+--    currency-greater-than: integer,
+--    currency-less-than: integer,
+--    exclude-close-to: boolean,
+--    limit: integer,
+--    max-round: integer,
+--    min-round: integer,
+--    next: string,
+--    note-prefix: string,
+--    rekey-to: boolean,
+--    round: integer,
+--    sig-type: enum (sig, msig, lsig),
+--    tx-type: enum (pay, keyreg, acfg, axfer, afrz, appl),
+--    txid: string,
+--  } | nil
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.block(round_number, on_success, on_error)
 	http_client.get(Indexer.address .. "/v2/blocks/" .. round_number, headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.transactions
+--  Search for transactions.
+-- @Input:
+--  - round_number: integer
+--  - params: table {
+--    address: integer,
+--    address-role: enum (sender, receiver, freeze-target),
+--    after-time: string (date-time),
+--    application-id: integer,
+--    asset-id: integer,
+--    before-time: string (date-time),
+--    currency-greater-than: integer,
+--    currency-less-than: integer,
+--    exclude-close-to: boolean,
+--    limit: integer,
+--    max-round: integer,
+--    min-round: integer,
+--    next: string,
+--    note-prefix: string,
+--    rekey-to: boolean,
+--    round: integer,
+--    sig-type: enum (sig, msig, lsig),
+--    tx-type: enum (pay, keyreg, acfg, axfer, afrz, appl),
+--    txid: string,
+--  } | nil
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.transactions(on_success, on_error)
-	--[[
-	@Optional parameters:
-	address: integer,
-	address-role: enum (sender, receiver, freeze-target),
-	after-time: string (date-time),
-	application-id: integer,
-	asset-id: integer,
-	before-time: string (date-time),
-	currency-greater-than: integer,
-	currency-less-than: integer,
-	exclude-close-to: boolean,
-	limit: integer,
-	max-round: integer,
-	min-round: integer,
-	next: string,
-	note-prefix: string,
-	rekey-to: boolean,
-	round: integer,
-	sig-type: enum (sig, msig, lsig),
-	tx-type: enum (pay, keyreg, acfg, axfer, afrz, appl),
-	txid: string,
-	]]
 	http_client.get(Indexer.address .. "/v2/transactions", headers(), on_success, on_error)
 end
 
+-- @Method:
+--  Indexer.transaction
+--  Lookup a single transaction.
+-- @Input:
+--  - txid: string
+--  - on_success: function (data) | nil
+--  - on_error: function (data) | nil
 function Indexer.transaction(txid, on_success, on_error)
 	http_client.get(Indexer.address .. "/v2/transactions/" .. txid, headers(), on_success, on_error)
 end
